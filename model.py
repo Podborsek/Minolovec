5c7dec1b6be5d7cc4a5e84898d17715d18624229
Podborsek <gasper.rotar@student.fmf.uni-lj.si>
2019-08-28 11:33:15 +0200

    Model je v grobem dokoncan

--- model.py
+++ model.py
@@ -1,21 +1,115 @@
 import math
 import random
 
+#Definiramo konstante
+VISINA = 10
+SIRINA = 8
+TEZAVNOST = 25
+ZMAGA = "+"
+PORAZ = "-"
+ODKRITA = "od"
+ZAKRITA = "zak"
+ZASTAVICA = "zas"
+NAPAKA = "eee"
+
+
 class Igra:
     '''Osnovni objekt, ki predstavlaj igro'''
 
     def __init__(self, visina, sirina, tezavnost, prvi_y = 0, prvi_x = 0):
-        #Od pomoznih funkcij prejmemo matrike in jih zdruzimo v slovar
-        matrika = polje_osnovno(visina, sirina, tezavnost, prvi_y = 0, prvi_x = 0)
-        pass
+        '''Od pomoznih funkcij prejmemo matrike in jih zdruzimo v slovar'''
+        self.visina = visina
+        self.sirina = sirina
+        self.prvi = (prvi_x,prvi_y)
+        self.tezavnost = tezavnost
+
+        matrika_os = polje_osnovno(visina, sirina, tezavnost, prvi_y = 0, prvi_x = 0)
+        matrika_so = polje_sosedi(matrika_os)
+
+        slovar = {}
+        #Slovar ima za kljuce koordiante, za vrednosti pa sezname oblike [bomba(T/F), sosede(0-8), odkritost(1/2/3)]
+        for x in range(sirina):
+            for y in range(visina):
+                slovar[(x,y)] = [matrika_os[y][x], matrika_so[y][x], ZAKRITA]
+        self.slovar = slovar
+    
+    def __str__(self):
+        sl = self.slovar
+        niz = ""
+        for y in range(self.visina):
+            vrsta = ""
+            for x in range(self.sirina):
+                (_,b,c) = sl[(x,y)]
+                if c == ODKRITA:
+                    vrsta += str(b)
+                elif c == ZAKRITA:
+                    vrsta += "X"
+                elif c == ZASTAVICA:
+                    vrsta += "¶"
+                vrsta += " "
+            niz += vrsta.strip() + "\n"
+        return niz.strip("\n")
+    
+    def odkrij(self, x, y):
+        sl = self.slovar
+        seznam = sl[(x,y)]
+        bomba = seznam[0]
+        sosede = seznam[1]
+        odkritost = seznam[2]
+
+        if odkritost == ODKRITA or odkritost == ZASTAVICA:
+            return None
+        else:
+            self.slovar[(x,y)] = [bomba,sosede,ODKRITA]
+        
+        if sosede == 0:
+            for (a,b) in [(x-1,y-1),(x, y-1),(x+1, y-1),(x-1,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)]:
+                if 0 <= a < self.sirina and 0 <= b < self.visina:
+                    self.odkrij(a,b)
+
+
+    def izkoplji(self, x, y):
+        seznam = self.slovar[(x,y)]
+        (bomba, sosede, stanje) = seznam
+
+        if stanje == ZASTAVICA or stanje == ODKRITA:
+            return NAPAKA
+        elif bomba == True:
+            return PORAZ
+        else:
+            self.odkrij(x,y)
+            return None
+
+    
+    def posadi(self, x, y):
+        stanje = self.slovar[(x,y)][2]
+
+        if stanje == ODKRITA:
+            return NAPAKA
+        elif stanje == ZAKRITA:
+            self.slovar[(x,y)] = self.slovar[(x,y)][:2] + [ZASTAVICA]
+            return None
+        elif stanje == ZASTAVICA:
+            self.slovar[(x,y)] = self.slovar[(x,y)][:2] + [ZAKRITA]
+            return None
+    
+    def zmaga(self):
+        stevilo_zastavic = 0
+        for seznam in self.slovar.values():
+            if seznam[2] == ZASTAVICA:
+                if not seznam[0]:
+                    return False
+                stevilo_zastavic += 1
+            
+        return stevilo_zastavic == self.tezavnost
 
 
 
 
 
-
-
-
+#-----------------------------------------------------------------------
+#------------------  P O M O Z N E    F U N K C I J E   ----------------
+#-----------------------------------------------------------------------
 
 
 def polje_osnovno(visina, sirina, tezavnost, prvi_y = 0, prvi_x = 0):
@@ -51,7 +145,6 @@ def polje_sosedi(matrika):
     for vrstica in matrika:
         oblazinjena.append([False] + vrstica + [False])
     oblazinjena.append([False for x in range(sirina + 2)])
-    print(oblazinjena)
 
     nova = []
     for y in range(1, visina + 1):
@@ -59,15 +152,32 @@ def polje_sosedi(matrika):
         for x in range(1, sirina + 1):
             sosede = 0
             for (a,b) in [(x-1,y-1),(x, y-1),(x+1, y-1),(x-1,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)]:
-                if oblazinjena[a][b]:
+                if oblazinjena[b][a]:
                     sosede += 1
             vrstica.append(sosede)
         nova.append(vrstica)
-    
+
     return nova
 
+
+
+
+#-----------------------------------------------------------
+#----------------------- T E S T I -------------------------
+#-----------------------------------------------------------
+
+
+
+
+qwert = Igra(10,10,20)
+for x in range(20):
+    qwert.odkrij(random.randrange(10), random.randrange(10))
+    qwert.posadi(random.randrange(10), random.randrange(10))
+    print(qwert)
+    print("\n")
     
 
-qwe = polje_osnovno(4,4,7)
-print(qwe)
-print(polje_sosedi(qwe))
+print(qwert)
+qwert.zmaga()
+
+
